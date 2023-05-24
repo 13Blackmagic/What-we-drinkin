05c2de837cb8e0bb699f95bcf8776e152d7d0765
13Blackmagic <123032739+13Blackmagic@users.noreply.github.com>
2023-05-23 20:58:18 -0600

    api fix

--- node.js
+++ node.js
@@ -1,9 +1,8 @@
-const axios = require('axios');
+const fetch = require('node-fetch');
 
+const url = 'https://the-cocktail-db.p.rapidapi.com/search.php?s=vodka';
 const options = {
   method: 'GET',
-  url: 'https://the-cocktail-db.p.rapidapi.com/search.php',
-  params: {s: 'vodka'},
   headers: {
     'X-RapidAPI-Key': 'a26b34a647mshb62184be136346cp11b221jsn7ecc0ba0fe47',
     'X-RapidAPI-Host': 'the-cocktail-db.p.rapidapi.com'
@@ -11,8 +10,9 @@ const options = {
 };
 
 try {
-	const response = await axios.request(options);
-	console.log(response.data);
+	const response = await fetch(url, options);
+	const result = await response.text();
+	console.log(result);
 } catch (error) {
 	console.error(error);
 }
