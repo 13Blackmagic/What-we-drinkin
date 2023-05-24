1858f3000602cc7cb24eec5cf697ea0ec6003398
13Blackmagic <123032739+13Blackmagic@users.noreply.github.com>
2023-05-24 10:36:10 -0600

    adding to schema.sql

---
+++ sql/schema.sql
@@ -0,0 +1,23 @@
+DROP DATABASE IF EXISTS liquor_db;
+CREATE DATABASE liquor_db;
+
+USE liquor_db;  
+
+CREATE TABLE brand (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE liquor (
+    id INT NOT NULL AUTO_INCREMENT,
+    name_id INT NOT NULL,
+    brand_id INT NOT NULL,
+    price DECIMAL(10,2) NOT NULL,
+    volume DECIMAL(10,2) NOT NULL,
+    type VARCHAR(255) NOT NULL,
+    description VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (name_id) REFERENCES name(id),
+    FOREIGN KEY (brand_id) REFERENCES brand(id)
+);
