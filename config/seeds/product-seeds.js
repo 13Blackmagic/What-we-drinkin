fa446fbd29da37cbaf38155684c8cd6aec4dea8f
13Blackmagic <123032739+13Blackmagic@users.noreply.github.com>
2023-06-01 10:41:05 -0600

    adding to product-seeds

---
+++ config/seeds/product-seeds.js
@@ -0,0 +1,32 @@
+const {product } = require('../../models'); 
+
+const productData = [
+    {
+        product_name: 'Jack Daniels',
+        price: 20.00,
+        stock: 10,
+        category_id: 1,
+        brand_id: 1,
+    },
+    {
+        product_name: 'Grey Goose',
+        price: 30.00,
+        stock: 10,
+        category_id: 2,
+        brand_id: 2,
+    },
+    {
+        product_name: 'Captain Morgan',
+        price: 20.00,
+        stock: 10,
+        category_id: 3,
+        brand_id: 3,
+    },
+    {
+        product_name: 'Jose Cuervo',
+        price: 20.00,
+        stock: 10,
+        category_id: 4,
+        brand_id: 4,
+    },
+    
