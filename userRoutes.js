ad45452fe70dd9fb01c1d2d0711b556bd71c4a38
13Blackmagic <123032739+13Blackmagic@users.noreply.github.com>
2023-06-01 11:14:02 -0600

    adding new users

--- userRoutes.js
+++ userRoutes.js
@@ -10,3 +10,51 @@ router.get('/', async (req, res) => {
     }
 }
 );
+
+router.post('/', async (req, res) => {
+    try {
+        const userData = await User.create(req.body);
+        res.status(200).json(userData);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+}
+);
+
+router.put('/:id', async (req, res) => {
+    try {
+        const userData = await User.update(req.body, {
+            where: {
+                id: req.params.id,
+            },
+        });
+        if (!userData[0]) {
+            res.status(404).json({ message: 'No user with this id!' });
+            return;
+        }
+        res.status(200).json(userData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+}
+);
+
+router.delete('/:id', async (req, res) => {
+    try {
+        const userData = await User.destroy({
+            where: {
+                id: req.params.id,
+            },
+        });
+        if (!userData) {
+            res.status(404).json({ message: 'No user with this id!' });
+            return;
+        }
+        res.status(200).json(userData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+}
+);
+
+module.exports = router;
