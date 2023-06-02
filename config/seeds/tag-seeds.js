706a3c78716ac98e4920dd39a9478d9d586452bc
Luceatscode <lucsw92@hotmail.com>
2023-06-01 19:58:52 -0600

    added modesl

--- config/seeds/tag-seeds.js
+++ config/seeds/tag-seeds.js
@@ -1,26 +1,26 @@
-const {Tag} = require('../models');
+// const {Tag} = require('../models');
 
-const tagData = [
-    {
-        tag_name: 'Whiskey'
-    },
-    {
-        tag_name: 'Vodka'
-    },
-    {
-        tag_name: 'Rum'
-    },
-    {
-        tag_name: 'Tequila'
-    },
-    {
-        tag_name: 'Gin'
-    },
-    {
-        tag_name: 'Brandy'
-    },
-];
+// const tagData = [
+//     {
+//         tag_name: 'Whiskey'
+//     },
+//     {
+//         tag_name: 'Vodka'
+//     },
+//     {
+//         tag_name: 'Rum'
+//     },
+//     {
+//         tag_name: 'Tequila'
+//     },
+//     {
+//         tag_name: 'Gin'
+//     },
+//     {
+//         tag_name: 'Brandy'
+//     },
+// ];
 
-const seedTags = () => Tag.bulkCreate(tagData);
+// const seedTags = () => Tag.bulkCreate(tagData);
 
-module.exports = seedTags;
+// module.exports = seedTags;
