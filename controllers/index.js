cb16a5ff4f656fde265d2dbc06a7faf31d287024
Joshua Myers <jt.myers976@gmail.com>
2023-06-05 21:35:56 -0600

    Fixed paths again.

--- controllers/index.js
+++ controllers/index.js
@@ -3,8 +3,8 @@ const router = require('express').Router();
 const homeRoutes = require('./home-routes.js');
 const userRoutes = require('./userRoutes');
 
-router.use('/', homeRoutes);
-router.use('/auth', userRoutes);
+router.use('/', userRoutes);
+router.use('/home', homeRoutes);
 
 
 module.exports = router;
